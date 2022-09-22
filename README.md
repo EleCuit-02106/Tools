7f84522f081a12a522086a2622a06255f209b02e
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2022-09-22 19:53:22 +0900

    feat: GoogleDrive上のスプシ一覧を取得する
    - README.md に実装の詳細を追記

--- README.md
+++ README.md
@@ -21,3 +21,57 @@ check and generate Master/UserClasses
 - repository_path.py
   - path.toml にて固有のパスを定義、それを読み出す
 
+
+## python から Google Drive へのアクセス方法
+
+### PyDrive2
+
+- 全体の流れ
+  - https://note.nkmk.me/python-pydrive-download-upload-delete/
+    - PyDrive1 時代の説明だが 2 でも大体同じ
+    - PyDrive1 のリンクは切れているので PyDrive2 に読み替えること
+      - GitHub: https://github.com/iterative/PyDrive2
+      - Docs::QuickStart: https://docs.iterative.ai/PyDrive2/quickstart/
+      - Docs::settings.yaml: https://docs.iterative.ai/PyDrive2/oauth/
+
+1. 疎通
+  1. Google Cloud の Develop Console にアクセス
+    - https://console.cloud.google.com/apis/dashboard?project=readmdbypython&supportedpurview=project
+  1. プロジェクト作成
+  1. Google Drive API の有効化
+    - スプレッドシートの内容を python で読み込むのに Google Sheets API も有効化しておくと良い
+      - cf. https://kino-code.com/python_spreadsheets/#Google
+  1. 左ペイン > 認証情報 > 認証情報を作成 > OAuthクライアントID と選択
+    - 初回はここで同意画面の作成に飛ばされるので飛ばされるので、表示に従い作成する
+  1. 終わるとダッシュボードに戻されるので再度「認証情報を作成 > OAuthクライアントID」を選択
+    - ドキュメント通りに進行、client_secretsXXXXXX.json をダウンロードする
+  - ここまで来たら、 client_secrets.json があるディレクトリで認証が可能
+    ~~~py
+    from pydrive2.auth import GoogleAuth
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    ~~~
+    - 実行するとブラウザが起動しGoogleの同意画面が表示される
+    - Google Cloud Console 上でアプリ公開していない場合はテストユーザしかアクセスできないので注意
+      1. Google Cloud Console > 左ペイン > OAuth同意画面 のテストユーザ欄の ADD USER からユーザを追加 (Googleアカウント)
+      1. 上記コード実行時に表示される同意画面上で、テストユーザに登録したGoogleアカウントを選択する
+1. サイレント認証
+  - 認証時にブラウザでの操作が要らないように設定する
+  1. 認証する python スクリプトと同じ場所に settings.yaml を作成
+  1. settings.yaml に以下を記入
+    ~~~yaml
+    save_credentials: True
+    save_credentials_backend: file
+    save_credentials_file: secrets/credentials.json # settings.yaml からの相対パス
+    ~~~
+    - 他にも色々設定できる
+      - https://note.nkmk.me/python-pydrive-download-upload-delete/
+        - サンプルが PyDrive1 向けなので注意
+      - https://docs.iterative.ai/PyDrive2/oauth/
+      - settings.yaml を変更した場合は python を再起動すること
+        - GoogleAuth() のインスタンスを作り直さないと変更が反映されず変なエラーが出たりする
+        - e.g. InvalidConfigError: Unknown client_config_backend
+
+
+
+
--- Schema/load_spreadsheet.py
+++ Schema/load_spreadsheet.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 #coding:utf-8
 
+import os
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import rapidjson
@@ -16,38 +19,70 @@ class MasterDataConverter:
     """
 
     NUMERIC_TYPES = ('int', 'float', 'unixtime')
+    GOOGLE_DRIVE_MD_PATH = "home/pro/develop/EleCuit/Data/MasterData"
 
     def __init__(self):
         logger.info('[MasterDataConverter] initialize ...')
         logger.info('[MasterDataConverter] authorize ...')
-        self.gs = self.authorize_and_get_gspread()
+        self.gs = self._authorize_and_get_gspread()
         logger.info('[MasterDataConverter] load file keys ...')
-        self.file_keys = self.load_file_keys()
+        self.file_keys = list()
         self.records = dict()
 
-    def authorize_and_get_gspread(self) -> gspread.Client:
-        KEY_JSON_PATH = '/Users/kazuaki/.ssh/readmdbypython-a352795e2e0a.json'
+    def _authorize_and_get_gspread(self) -> gspread.Client:
+        tool_dir = ProjectPath.absolute('tool')
+        key_json_path = tool_dir / 'Schema/secrets/gspread_key.json'
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive']
-        c = ServiceAccountCredentials.from_json_keyfile_name(KEY_JSON_PATH, scope)
+        c = ServiceAccountCredentials.from_json_keyfile_name(key_json_path, scope)
         return gspread.authorize(c)
 
-    def load_file_keys(self) -> list[str]:
-        file_keys = ['1Cd3ASeYgy7XOFFU0V1Hj0DGK9oh1g632ShAmpEmtvXc']
-        return file_keys
+    def load_file_keys(self) -> None:
+        self.g_auth = GoogleAuth()
+        self.g_auth.LocalWebserverAuth()
+        self.g_drive = GoogleDrive(self.g_auth)
+        g_drive_md_path_as_list = self.GOOGLE_DRIVE_MD_PATH.split('/')
+        md_root_dir = self._search_dir(self.g_drive, g_drive_md_path_as_list)
+        logger.info('[MasterDataConverter.SearchFilesAtGoogleDrive] MasterData/')
+        self._load_file_keys_impl(md_root_dir)
+        # self.file_keys = ['1Cd3ASeYgy7XOFFU0V1Hj0DGK9oh1g632ShAmpEmtvXc']
+
+    def _load_file_keys_impl(self, dir_id:str, log_indent:int=1) -> None:
+        children = self.g_drive.ListFile({'q': ('"%s" in parents' % dir_id)}).GetList()
+        for child in children:
+            if child['mimeType'] == 'application/vnd.google-apps.folder':
+                logger.info('[MasterDataConverter.SearchFilesAtGoogleDrive] %s%s/' % ('  ' * log_indent, child['title']))
+                self._load_file_keys_impl(child['id'], log_indent + 1)
+            elif child['mimeType'] == 'application/vnd.google-apps.spreadsheet':
+                logger.info('[MasterDataConverter.SearchFilesAtGoogleDrive] %s%s' % ('  ' * log_indent, child['title']))
+                self.file_keys.append(child['id'])
+
+    def _search_dir(self, drive:GoogleDrive, absolute_path:list[str]) -> str:
+        parent_dir = 'root'
+        for next_dir in absolute_path:
+            searched_dir = drive.ListFile({'q': ('"%s" in parents and title = "%s"' % (parent_dir, next_dir))}).GetList()
+            if len(searched_dir) != 1:
+                logger.critical('[MasterDataConverter.SearchFilesAtGoogleDrive] No such directory: %s (%s)' % (next_dir, absolute_path))
+                logger.critical('[MasterDataConverter.SearchFilesAtGoogleDrive] Exist directories: %s' % searched_dir)
+                return None
+            parent_dir = searched_dir[0]['id']
+        return parent_dir
 
     def load_spreadsheets(self) -> None:
         self.records.clear()
+        loaded_files = 0
         for file_key in self.file_keys:
             for worksheet in self.gs.open_by_key(file_key).worksheets():
-                self.worksheet_to_json(worksheet)
+                self._worksheet_to_json(worksheet)
+            loaded_files += 1
+            logger.info('[MasterDataConverter.ConvertToJson] converted %d / %d files' % (loaded_files, len(self.file_keys)))
 
-    def worksheet_to_json(self, worksheet:gspread.Worksheet):
+    def _worksheet_to_json(self, worksheet:gspread.Worksheet):
         class_name = worksheet.title
         if not class_name in self.records.keys():
             self.records[class_name] = dict()
         field_names = worksheet.row_values(1)
         field_types = worksheet.row_values(2)
-        id_column = self.search_id_column(field_names)
+        id_column = self._search_id_column(field_names)
         row = 4 # マスタデータのレコードは4行目から
         while True:
             record = worksheet.row_values(row)
@@ -67,7 +102,7 @@ class MasterDataConverter:
             row += 1
 
     # 見つからなかった場合 -1 を返す
-    def search_id_column(self, field_names:list[str]):
+    def _search_id_column(self, field_names:list[str]):
         column = 1
         for field_name in field_names:
             if field_name == 'id':
@@ -83,7 +118,11 @@ class MasterDataConverter:
 
 if __name__ == "__main__":
     basicConfig(level=INFO)
+    #  認証情報ファイルがカレントディレクトリにある必要があるので実行ディレクトリを指定する
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     mdConverter = MasterDataConverter()
+    logger.info('[MasterDataConverter] load master data file list ...')
+    mdConverter.load_file_keys()
     logger.info('[MasterDataConverter] load master data ...')
     mdConverter.load_spreadsheets()
     logger.info('[MasterDataConverter] dump json ...')
