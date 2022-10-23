3795d2ed9281b72e4257ea3e228dc46d879b2041
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2022-10-23 20:13:02 +0900

    feat: 整形してから.json出力

--- Schema/load_spreadsheet.py
+++ Schema/load_spreadsheet.py
@@ -117,7 +117,7 @@ class MasterDataConverter:
         dest_dir = ProjectPath.absolute('md_json')
         for md_type in self.records:
             dest_file = 'Master%s.json' % md_type
-            output(dest_dir / dest_file, rapidjson.dumps(self.records[md_type]))
+            output(dest_dir / dest_file, rapidjson.dumps(self.records[md_type], indent=4))
 
 if __name__ == "__main__":
     basicConfig(level=INFO)
